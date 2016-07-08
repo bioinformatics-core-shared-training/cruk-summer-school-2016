90825fd2465fcbc7a8a323fc67a5c46e6a388a32
markdunning <mark.dunning@gmail.com>
2016-07-08 13:29:12 +0100

    add limma to names of packages

--- installBioCPkgs.R
+++ installBioCPkgs.R
@@ -2,9 +2,9 @@ source("http://www.bioconductor.org/biocLite.R")
 options(BioC_mirror = c("Cambridge" = "http://mirrors.ebi.ac.uk/bioconductor/"))
 options(repos = c("CRAN" = "http://cran.ma.imperial.ac.uk"))
 biocLite(c("Biostrings", "biomaRt", "BSgenome","QDNAseq","exomeCopy",
-        "rtracklayer", "ggbio", "vsn","gplots","RColorBrewer","htSeqTools","org.Hs.eg.db",
+        "rtracklayer", "ggbio", "Gviz","RColorBrewer","org.Hs.eg.db",
         "TxDb.Hsapiens.UCSC.hg19.knownGene","BSgenome.Hsapiens.UCSC.hg19",
-        "wakefield","VariantAnnotation"))
+        "wakefield","VariantAnnotation","limma"))
 #packages for ChIP on Day 4        
 
 
