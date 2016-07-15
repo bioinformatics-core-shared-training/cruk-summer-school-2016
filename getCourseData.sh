a252eefa54395d5d260eaf574187af15b5390a82
markdunning <mark.dunning@gmail.com>
2016-07-15 09:44:06 +0100

    update paths in course install script

--- getCourseData.sh
+++ getCourseData.sh
@@ -1,19 +1,27 @@
 ## Get hapmap examples genotype calling 
 
 chmod +x data/hapmap/download.sh
-./data/hapmap/download.sh
+cd data/hapmap/
+./download.sh
+cd ../../
 
 ## Get downsampled HCC1143 cell-line from Sanger
 
 chmod +x data/cell-line/downsampled/download.sh
-./data/cell-line/downsampled/download.sh
+cd data/cell-line/downsampled/
+./download.sh
+cd ../../
 
 ## Get example bam and fastq files for Day 1
 
 chmod +x Day1/data_for_day1.sh
-./Day1/data_for_day1.sh
+cd Day1
+./data_for_day1.sh
+cd ../
 
 ## 
 
 chmod +x ref_data/download.sh
-./ref_data/download.sh
+cd ref_data
+./download.sh
+cd ../
