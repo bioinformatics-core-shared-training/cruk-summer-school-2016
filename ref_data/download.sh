7ac7ceb0a882567b337fcc87c9b9975ce53d0ded
markdunning <mark.dunning@gmail.com>
2016-07-15 12:14:14 +0100

    download the genome index file too

--- ref_data/download.sh
+++ ref_data/download.sh
@@ -1,2 +1,3 @@
 wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/human_g1k_v37.fasta.gz
+wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/human_g1k_v37.fasta.fai
 gunzip human_g1k_v37.fasta.gz 
