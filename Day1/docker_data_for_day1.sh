54d5108e9732f01bd54e868f12f2ec79bee76824
markdunning <mark.dunning@gmail.com>
2016-07-15 11:44:59 +0100

    typo in day 1 script

--- Day1/docker_data_for_day1.sh
+++ Day1/docker_data_for_day1.sh
@@ -5,9 +5,11 @@ wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/other_exome_alignments/N
 
 java -jar $PICARD DownsampleSam I=NA06984.mapped.illumina.mosaik.CEU.exome.20111114.bam O=random.bam P=0.1 VALIDATION_STRINGENCY=SILENT
 
-rm NA12878.mapped.ILLUMINA.bwa.CEU.exome.20121211.bam
+rm NA06984.mapped.illumina.mosaik.CEU.exome.20111114.bam
 
 samtools view -f 0x02 -b random.bam > paired.bam 
+
+samtools index paired.bam
 
 java -jar $PICARD SamToFastq I=paired.bam F=sample.fq1 VALIDATION_STRINGENCY=SILENT F2=sample.fq2 R1_MAX_BASES=68 R2_MAX_BASES=68
 
