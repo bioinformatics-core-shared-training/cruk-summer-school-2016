4dd4850ee5162421a6586356c976182bfd219b08
markdunning <mark.dunning@gmail.com>
2016-07-20 10:08:06 +0100

    add a couple of R packages to list

--- installBioCPkgs.R
+++ installBioCPkgs.R
@@ -1,8 +1,8 @@
 source("http://www.bioconductor.org/biocLite.R")
-options(BioC_mirror = c("Cambridge" = "http://mirrors.ebi.ac.uk/bioconductor/"))
+  options(BioC_mirror = c("Cambridge" = "http://mirrors.ebi.ac.uk/bioconductor/"))
 options(repos = c("CRAN" = "http://cran.ma.imperial.ac.uk"))
 biocLite(c("Biostrings", "biomaRt", "BSgenome","QDNAseq","exomeCopy",
         "rtracklayer", "ggbio", "Gviz","RColorBrewer","org.Hs.eg.db",
         "TxDb.Hsapiens.UCSC.hg19.knownGene","BSgenome.Hsapiens.UCSC.hg19",
-        "wakefield","VariantAnnotation","limma","dplyr","ggplot2","tidyr","readr"))
+        "wakefield","VariantAnnotation","limma","dplyr","ggplot2","tidyr","readr","RCircos","GenVisR"))
 
